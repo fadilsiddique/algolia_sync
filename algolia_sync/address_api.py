bbaf2a92e642ad37e7f085c35e2c8647809bdf7b
hafnianusha <hafnianusha@tridz.com>
2022-05-05 07:47:01 +0000

    image-filter

--- algolia_sync/address_api.py
+++ algolia_sync/address_api.py
@@ -9,3 +9,8 @@ def address(email):
     address = frappe.db.get_all("Address",filters={"owner":email,"disabled":0},fields=["name","address_title","disabled","address_line1","address_line2","city","state","country","pincode","phone"])
     return address
         # return address
+
+@frappe.whitelist()
+def ItemFilter(item_code):
+    Items = frappe.db.get_all("Item",filters = {"item_code":item_code},fields=["image"])
+    return Items
