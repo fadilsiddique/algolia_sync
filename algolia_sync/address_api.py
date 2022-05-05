ce927c4b93134fd5e7628bcea5e1f1006e710b5d
hafnianusha <hafnianusha@tridz.com>
2022-05-05 05:37:11 +0000

    disabled_0

--- algolia_sync/address_api.py
+++ algolia_sync/address_api.py
@@ -6,5 +6,6 @@ import requests
 
 @frappe.whitelist()
 def address(email):
-    address = frappe.db.get_all("Address",filters={"owner":email},fields=["name","address_title","address_line1","address_line2","city","state","country","pincode","phone"])
+    address = frappe.db.get_all("Address",filters={"owner":email,"disabled":0},fields=["name","address_title","disabled","address_line1","address_line2","city","state","country","pincode","phone"])
     return address
+        # return address
