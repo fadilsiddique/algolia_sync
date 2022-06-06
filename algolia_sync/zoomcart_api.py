1a5389da7cf44d3c6b92a0cbcda46ba0269dbcb7
hafnianusha <hafnianusha@tridz.com>
2022-06-06 08:01:30 +0000

    Add:Sales order status

--- algolia_sync/zoomcart_api.py
+++ algolia_sync/zoomcart_api.py
@@ -23,13 +23,13 @@ def order():
     sales_item = []
     order_item = []
     so = frappe.db.get_all("Sales Order",filters = {"contact_email":user},fields=["order_status","name","transaction_date","delivery_date","grand_total",\
-    "total_taxes_and_charges"])
+    "total_taxes_and_charges","status"])
     for i in so:
         sales_order = frappe.get_doc("Sales Order",i["name"])
         for item in sales_order.items:
             imageItem = frappe.get_doc('Item',item.item_code)
             order_item.append({"Item_name":item.item_name,"item_code":item.item_code,"qty":item.qty,"price":item.amount,"Image":imageItem.website_image_1,"order_status":i["order_status"],\
-            "transaction_date":i["transaction_date"],\
+            "transaction_date":i["transaction_date"],"status":i["status"],\
             "delivery_date":i["delivery_date"],"id":i["name"]})     
 
         sales_item.append({"sales_invoice_name":i["name"],"Grand Total":i["grand_total"],"Tax":i["total_taxes_and_charges"],"Response":order_item})
