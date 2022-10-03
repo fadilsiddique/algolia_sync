0418dcc5ce7c27261da2aa8a29a22cea7b9c14df
fadilsid <fadil@tridz.com>
2022-10-03 17:52:04 +0000

    fix:reference in address api

--- algolia_sync/zoomcart_api.py
+++ algolia_sync/zoomcart_api.py
@@ -6,7 +6,8 @@ import requests
 
 @frappe.whitelist()
 def address(email):
-    address = frappe.db.get_all("Address",filters={"owner":email,"disabled":0})
+    address = frappe.db.get_all("Address",filters={"owner":email,"disabled":0},fields=["name","address_title","disabled",\
+    "address_line1","address_line2","city","state","country","pincode","phone","links"])
     return address
         # return address
 
